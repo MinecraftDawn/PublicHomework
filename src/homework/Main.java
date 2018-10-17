e89fb2d58cbe561fbe03fca5ecd49f242377e1b6
MinecraftDawn <eric4902077@gmail.com>
2018-10-17 21:43:05 +0800

    作業一

---
+++ src/homework/Main.java
@@ -0,0 +1,39 @@
+package homework;
+
+import java.util.Scanner;
+
+public class Main {
+    private static String str = "";
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("請輸入一整數");
+        int num = scanner.nextInt();
+
+        boolean prime = true;
+
+        int copyNum = num;
+        for (int i = 2; i <= copyNum; i++) {
+            if (num % i == 0){
+                while(true){
+                    if(copyNum % i == 0){
+                        copyNum /= i;
+                        str += i + " ";
+                    }else{
+                        break;
+                    }
+                }
+                prime = false;
+            }
+        }
+
+        if(prime){
+            System.out.println(num +"是質數");
+        }else{
+            System.out.println(num +"不是質數");
+            System.out.println(str);
+        }
+
+    }
+
+}

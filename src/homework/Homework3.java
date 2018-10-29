cb2bd7a479a890e04a8d7bd6dc11ff96b27efc46
MinecraftDawn <eric4902077@gmail.com>
2018-10-29 19:57:23 +0800

    猜數字(有Bug)

--- src/homework/Homework3.java
+++ src/homework/Homework3.java
@@ -7,15 +7,34 @@ public class Homework3 {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         String answer = "";
+        String input;
         int num;
         for (int i = 0; i < 4; i++) {
             do {
                 num = (new Random()).nextInt(10);
-            }while (answer.contains(String.valueOf(num)));
+            } while (answer.contains(String.valueOf(num)));
 
             answer += num;
         }
 
+
+        for (int i = 0; i < 10; i++) {
+            int A = 0;
+            int B = 0;
+            input = scanner.nextLine();
+            for (int j = 0; j < 4; j++) {
+                if (input.charAt(j) == answer.charAt(j)) {
+                    A++;
+                }
+
+                for (int k = j + 1; k < 4; k++) {
+                    if (input.charAt(j) == answer.charAt(k)) {
+                        B++;
+                    }
+                }
+            }
+            System.out.println(A + "A; " + B + "B");
+        }
 
 
     }
